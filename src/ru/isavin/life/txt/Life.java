7749d18f6cd912f29b47fdbc0e5c513e6afbd678
saviily <sawa_83@mail.ru>
2015-02-03 22:42:39 +0300

    Main class

---
+++ src/ru/isavin/life/txt/Life.java
@@ -0,0 +1,14 @@
+package ru.isavin.life.txt;
+
+/**
+ * Created by ilasavin on 03.02.15.
+ */
+public class Life {
+
+    public static void main(String[] args) {
+        World world = new World();
+        world.populateWorld();
+        world.printWorld();
+    }
+
+}
